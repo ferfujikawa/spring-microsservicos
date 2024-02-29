5ced3de247e78088cf4740ee181546c53a7e509c
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-29 13:22:07 -0300

    Criação dos projetos estoque-api, gateway e service-discovery.

---
+++ estoque-api/src/test/java/com/fujikawa/springmicrosservicos/estoqueapi/EstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springmicrosservicos.estoqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
