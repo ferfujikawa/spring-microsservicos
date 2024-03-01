fd968284668ea17585f3384c788042f335799a08
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-01 09:35:36 -0300

    Adição de configurações de propriedades no arquivo application.properties e criação de classes de configuração com beans referentes ao consumo mensagens do RabbitMQ.

---
+++ estoque-api/src/main/java/com/fujikawa/springmicrosservicos/estoqueapi/amqp/EstoqueBaixarConsumeAMQPConfiguration.java
@@ -0,0 +1,58 @@
+package com.fujikawa.springmicrosservicos.estoqueapi.amqp;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EstoqueBaixarConsumeAMQPConfiguration {
+
+    private String exchange;
+    private String queue;
+
+    public EstoqueBaixarConsumeAMQPConfiguration(
+        @Value("${amqp.consume.estoqueBaixar.exchange}") String exchange,
+        @Value("${amqp.consume.estoqueBaixar.queue}") String queue) {
+
+        this.exchange = exchange;
+        this.queue = queue;
+    }
+
+    public String getExchange() {
+        return exchange;
+    }
+
+    public String getQueue() {
+        return queue;
+    }
+
+    @Bean
+    public Queue fila() {
+
+        return QueueBuilder
+            .nonDurable(queue)
+            .build();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+
+        return ExchangeBuilder
+            .fanoutExchange(exchange)
+            .build();
+    }
+
+    @Bean
+    public Binding binding() {
+
+        return BindingBuilder
+            .bind(fila())
+            .to(fanoutExchange());
+    }
+}
